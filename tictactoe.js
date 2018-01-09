b56098eb25a702748f99247cac1bd14c9828f3fb
lbarstow <lbarstow@macalester.edu>
2018-01-09 14:33:53 -0500

    "commit before attempting AI"

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,12 @@
-let cells = [[null, null, null],
-            [null, null, null],
-            [null, null, null]];
-let mark = "x";
+let cells;
+
+let mark;
 
 let init = () => {
-  console.log("init");
+  cells = [[null, null, null],
+            [null, null, null],
+            [null, null, null]];
+  mark = "x";
   drawCanvas();
 }
 
@@ -17,14 +19,34 @@ let onClick = (event) =>{
   if(cells[yIndex][xIndex]!==null ){
     return;
   }
-
   cells[yIndex][xIndex]= mark;
   if(mark === "x"){
     drawX(xIndex, yIndex);
+    checkForWin(xIndex, yIndex);
+    mark = "o";
   }else{
     drawO(xIndex, yIndex);
+    checkForWin(xIndex, yIndex);
+    mark = "x";
   }
 }
+
+let checkForWin = (x, y) =>{
+  //used else if because, while multiple branches could occur, it's only necessary for one to be
+  if(cells[0][x] === mark && cells[1][x] === mark && cells[2][x] === mark){
+    console.log("first")
+  }
+  else if (cells[y][0] === mark && cells[y][1] === mark && cells[y][2] === mark){
+    console.log("second")
+  }
+  else if(cells[0][0] === mark && cells[1][1] === mark && cells[2][2] === mark){
+    console.log("third")
+  }
+  else if(cells[2][0] === mark && cells[1][1] === mark && cells[0][2] === mark){
+    console.log("fourth")
+  }
+}
+
 let drawO = (x, y) =>{
   let startX = x * 100 + 50;
   let startY = y * 100 + 50;
@@ -33,15 +55,6 @@ let drawO = (x, y) =>{
   context.beginPath();
   context.arc(startX, startY, 40,0,2*Math.PI);
   context.stroke();
-  if(cells[0][x] === mark && cells[1][x] === mark && cells[2][x] === mark){
-    console.log("win")
-  }
-  if(cells[y][0] === mark && cells[y][1] === mark && cells[y][2] === mark){
-    console.log("win")
-  }
-  mark = "x";
-
-
 }
 let drawX = (x, y) =>{
   let startX = x * 100 + 5;
@@ -55,14 +68,6 @@ let drawX = (x, y) =>{
   context.moveTo(startX, startY +90);
   context.lineTo(startX + 90, startY);
   context.stroke();
-  if(cells[0][x] === mark && cells[1][x] === mark && cells[2][x] === mark){
-    console.log("win")
-  }
-  if(cells[y][0] === mark && cells[y][1] === mark && cells[y][2] === mark){
-    console.log("win")
-  }
-  //context.lineWidth = 1;
-  mark = "o";
 }
 
 let drawCanvas = () =>{
@@ -94,7 +99,9 @@ let drawCanvas = () =>{
 let reset = () => {
   let canvas = document.getElementById('tic_canvas');
   let body = document.getElementsByTagName('body')[0];
-  body.removeChild(canvas)
+  body.removeChild(canvas);
+
+
 
   init();
 }
